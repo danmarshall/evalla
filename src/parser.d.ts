e8c588b3e4835d4c6976a70dea2d335cd5c51158
danmarshall <danmarshall@users.noreply.github.com>
2026-02-04 22:32:48 +0000

    deploy: 40fef5307cb40535f241a58e37ff1723d4952b20

--- src/parser.d.ts
+++
@@ -1,9 +0,0 @@
-// Type definitions for the Peggy-generated parser
-export interface SyntaxError extends Error {
-  location: {
-    start: { offset: number; line: number; column: number };
-    end: { offset: number; line: number; column: number };
-  };
-}
-
-export function parse(input: string): any;
